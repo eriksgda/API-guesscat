46f909fd3501e96a5bb5355b738af586f3a4de65
eriksgda <erik.ksgda@gmail.com>
2025-01-21 17:42:54 -0300

    Start files

---
+++ src/main/java/com/eriksgda/guessCat/GuessCatApplication.java
@@ -0,0 +1,13 @@
+package com.eriksgda.guessCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eriksgda/guessCat/GuessCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriksgda.guessCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

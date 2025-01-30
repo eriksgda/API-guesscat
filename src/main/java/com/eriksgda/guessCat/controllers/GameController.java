9a092b069343144a00dc1d6debfc3dc8494b9793
eriksgda <erik.ksgda@gmail.com>
2025-01-30 18:21:02 -0300

    feat: add register game match endpoint

--- src/main/java/com/eriksgda/guessCat/controllers/GameController.java
+++ src/main/java/com/eriksgda/guessCat/controllers/GameController.java
@@ -1,14 +1,15 @@
 package com.eriksgda.guessCat.controllers;
 
 import com.eriksgda.guessCat.exceptions.FileIsEmptyException;
-import com.eriksgda.guessCat.model.historic.GameWordResponseDTO;
+import com.eriksgda.guessCat.exceptions.UserDoesNotExistException;
+import com.eriksgda.guessCat.model.game.Game;
+import com.eriksgda.guessCat.model.game.GameMatchDTO;
+import com.eriksgda.guessCat.model.game.GameWordResponseDTO;
 import com.eriksgda.guessCat.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -26,6 +27,18 @@ public class GameController {
             return ResponseEntity.ok().body(result);
         } catch (IOException | FileIsEmptyException exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error.");
+        }
+    }
+
+    @PostMapping("/done")
+    public ResponseEntity<?> registerGameMatch(@RequestBody GameMatchDTO data){
+        try{
+            Game result = this.gameService.registerGamePlayed(data);
+            return ResponseEntity.ok().body(result);
+        } catch (UserDoesNotExistException exception) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error.");
         }

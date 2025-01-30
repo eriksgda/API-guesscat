68296fa1a3848cd7ec813e77e31e75be48633ec0
eriksgda <erik.ksgda@gmail.com>
2025-01-30 18:17:40 -0300

    feat: add registerGamePlayed method

--- src/main/java/com/eriksgda/guessCat/services/GameService.java
+++ src/main/java/com/eriksgda/guessCat/services/GameService.java
@@ -1,9 +1,13 @@
 package com.eriksgda.guessCat.services;
 
-import com.eriksgda.guessCat.model.historic.GameWordResponseDTO;
+import com.eriksgda.guessCat.model.game.Game;
+import com.eriksgda.guessCat.model.game.GameMatchDTO;
+import com.eriksgda.guessCat.model.game.GameWordResponseDTO;
 
 import java.io.IOException;
 
 public interface GameService {
     GameWordResponseDTO startNewGame() throws IOException;
+
+    Game registerGamePlayed(GameMatchDTO data);
 }

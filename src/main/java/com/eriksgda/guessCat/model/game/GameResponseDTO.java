ea72fca939fc7b950b4eddf46e2a3e9428e3b411
eriksgda <erik.ksgda@gmail.com>
2025-02-03 18:58:38 -0300

    feat: create game response DTO

---
+++ src/main/java/com/eriksgda/guessCat/model/game/GameResponseDTO.java
@@ -0,0 +1,25 @@
+package com.eriksgda.guessCat.model.game;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record GameResponseDTO(
+        UUID id,
+        String word,
+        List<String> guesses,
+        LocalDateTime playedIn,
+        UUID playerId,
+        String playerUsername
+) {
+    public static GameResponseDTO fromEntity(Game game) {
+        return new GameResponseDTO(
+                game.getId(),
+                game.getWord(),
+                game.getGuesses(),
+                game.getPlayedIn(),
+                game.getPlayer().getId(),
+                game.getPlayer().getUsername()
+        );
+    }
+}

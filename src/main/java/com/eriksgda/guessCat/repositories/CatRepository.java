a7649877a9bccd55cfe3dd066453f21ff83c04dd
eriksgda <erik.ksgda@gmail.com>
2025-02-06 19:17:01 -0300

    feat: add exists by username method in repository

--- src/main/java/com/eriksgda/guessCat/repositories/CatRepository.java
+++ src/main/java/com/eriksgda/guessCat/repositories/CatRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 @Repository
 public interface CatRepository extends JpaRepository<Cat, UUID> {
     UserDetails findByUsername(String username);
+    boolean existsByUsername(String username);
 }

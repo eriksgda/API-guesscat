b63e58a46716c00f197e88d6d78e268d1abaf09c
eriksgda <erik.ksgda@gmail.com>
2025-02-06 19:16:36 -0300

    feat: add update dto with jakarta validation (Not Blank and Size)

---
+++ src/main/java/com/eriksgda/guessCat/model/cats/UpdateDTO.java
@@ -0,0 +1,16 @@
+package com.eriksgda.guessCat.model.cats;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateDTO(
+
+        @NotBlank(message = "Username cannot be blank.")
+        @Size(min = 3, max = 80, message = "Username must be between 3 and 80 characters.")
+        String newUsername,
+
+        @NotBlank(message = "Password cannot be blank.")
+        @Size(min = 3, max = 80, message = "Password must be between 3 and 80 characters.")
+        String newPassword
+) {
+}

34264e8ce4ad0a9a202d9236ad34ecbb1da421d5
eriksgda <erik.ksgda@gmail.com>
2025-08-14 21:40:01 -0300

    feat: add total points as a column and attribute to users

--- src/main/java/com/eriksgda/guessCat/model/cats/Cat.java
+++ src/main/java/com/eriksgda/guessCat/model/cats/Cat.java
@@ -22,18 +22,24 @@ public class Cat {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false, unique = true, length = 80)
+    @Column(nullable = false, unique = true, length = 12)
     private String username;
 
     @Column(nullable = false, length = 80)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private CatsRoles role;
 
     @OneToMany(mappedBy = "player", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Game> historic;
 
+    @Builder.Default
+    @Column(name="total_points" ,nullable = false)
+    private Long totalPoints = 0L; //default
+
     @CreationTimestamp
-    @Column(name = "created_at")
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 }
